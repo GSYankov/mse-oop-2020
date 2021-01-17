98961aa1cd34426354505e6507bf7d2d58281fe3
ivo.rusev <ivo.rusev@musala.com>
2021-01-17 14:25:32 +0200

    selenium crawl

---
+++ threads/src/main/java/com/mse/threads/crawler/SeleniumCrawler.java
@@ -0,0 +1,24 @@
+package com.mse.threads.crawler;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumCrawler {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\tmp\\selenium\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--window-size=1400,800");
+		WebDriver driver = new ChromeDriver(options);
+		driver.get("https://www.monster.com/jobs/q-administrative-jobs?jobid=f67acfbc-bfff-46bf-9c8f-f8e70b3b3721");
+		WebElement findElement = driver.findElement(
+				By.cssSelector("#JobDescription > table:nth-child(1) > tbody > tr > td > div > b:nth-child(1)"));
+		System.out.println(findElement.getText());
+	}
+
+}
